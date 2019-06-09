35e4d86078d18dae740fdc799cafe290d75dd21e
Victor Alveflo <victor.alveflo@gmail.com>
2019-06-09 14:10:47 +0200

    Save pages into /data

--- src/entities/page.go
+++ src/entities/page.go
@@ -1,19 +1,28 @@
 package entities
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"os"
+)
 
 type Page struct {
 	Title string
 	Body  []byte // byte slices: https://blog.golang.org/go-slices-usage-and-internals
 }
 
+var path = "./data/"
+
 func (p *Page) Save() error {
-	filename := p.Title + ".txt"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, 0600)
+	}
+
+	filename := path + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func LoadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := path + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return nil, err
